62a6b378bea21959205e28332b0c8dc8d750361d
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-07 17:26:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+    // Write your code here!
+    if (feet <= 400) {
+        return 'This one is on me!';
+    } else if (feet >= 2000 && feet <= 2500) {
+        return 'I will gladly take your thirty bucks.';
+    } else if (feet >= 2500) {
+        return 'No can do.';
+    }
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+    // Write your code here!
+    const result = (city == 'NYC') ? 'Ok, sounds good.' : 'No go.';
+    return result;
 }
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    switch (tip) {
+        case 'generous':
+            return 'Thank you so much.'
+            break;
+        case 'not as generous':
+            return "Thank you."
+            break;
+        default:
+            return 'Bye.'
+            break;
+    }
 }
+switchOnCharmFromTip('generous')
+
+switchOnCharmFromTip(' not as generous')
